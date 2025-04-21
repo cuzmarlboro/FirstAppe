18bbfafb931f51391b6908a9d80639ea5c9e07e0
hezeying <357261045@qq.com>
2025-04-21 20:38:11 +0800

    Initial Commit

---
+++ FirstAppe/FirstAppeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppeApp.swift
+//  FirstAppe
+//
+//  Created by ZHJY on 2025/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
